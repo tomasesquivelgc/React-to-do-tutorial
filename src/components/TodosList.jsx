a9a83221eebc766ee3af715aac9ac7439db3cd66
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-06-14 13:52:12 -0300

    Added components

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+const TodosList = ({ todosProps }) => (
+  <ul>
+    {todosProps.map((todo) => (
+      <TodoItem key={todo.id} itemProp={todo} />
+    ))}
+  </ul>
+);
+
+TodosList.propTypes = {
+  todosProps: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default TodosList;
